1351bd1a5b0aa2c98b13aaa79c56179b21cce7b4
Felipe <mmillanpelaez@danielcastelao.org>
2025-10-19 05:47:53 +0200

    Refactor area calculator into its own function in Boletin4

--- Boletin4.py
+++ Boletin4.py
@@ -8,56 +8,59 @@ print("Ejercicio 1")
 #> 1000 				primeira necesidade
 
 # 2. Area calculator
-print("Ejercicio 2")
-while True:
+def boletin4_2():
 
-    # print table
-    print ("\n--Calculador de Areas--")
-    print("1. Rectangulo")
-    print("2. Triángulo")
-    print("3. Círculo")
-    print("4. Salir")
+    print("\n--- Ejercicio 2 ---")
+    print("Calculador de áreas de figuras geométricas.")
 
-    # ask input
-    option = input("Selecciona un número: ")
+    while True:
 
-    # rectangle
-    if option == "1":
-        def area_rectangle():
-            length_str = input("Escribe el largo del rectángulo: ")
-            width_str = input("Escribe el ancho del rectángulo: ")
-            lenght = float(length_str)
-            width = float(width_str)
-            area = lenght*width
-            print("El area del rectángulo es:", area)
-        area_rectangle()
+        # print table
+        print ("\n--Calculador de Areas--")
+        print("1. Rectangulo")
+        print("2. Triángulo")
+        print("3. Círculo")
+        print("4. Salir")
 
-    # triangle
-    elif option == "2":
-        def area_triangle():
-            base_str = input("Escribe la base del triángulo: ")
-            height_str = input("Escribe la altura del triángulo: ")
-            base = float(base_str)
-            height = float(height_str)
-            area = base*height
-            print("El area del triángulo es:", area)
-        area_triangle()
+        # ask input
+        option = input("Selecciona un número: ")
 
-    # circle
-    elif option == "3":
-        def area_circle():
-            from math import pi
-            radius_str = input("Escribe el radio del círculo: ")
-            radius = float(radius_str)
-            area = pi*radius**2
-            print("El area del círculo es:", area)
-        area_circle()
+        # rectangle
+        if option == "1":
+            def area_rectangle():
+                length_str = input("Escribe el largo del rectángulo: ")
+                width_str = input("Escribe el ancho del rectángulo: ")
+                lenght = float(length_str)
+                width = float(width_str)
+                area = lenght*width
+                print("El area del rectángulo es:", area)
+            area_rectangle()
 
-    # break option
-    elif option == "4":
-        print("¡Hasta la vista!")
-        break
+        # triangle
+        elif option == "2":
+            def area_triangle():
+                base_str = input("Escribe la base del triángulo: ")
+                height_str = input("Escribe la altura del triángulo: ")
+                base = float(base_str)
+                height = float(height_str)
+                area = base*height
+                print("El area del triángulo es:", area)
+            area_triangle()
 
-    # else print error
-    else: print("Error: Por favor elija un valor correcto.")
+        # circle
+        elif option == "3":
+            def area_circle():
+                from math import pi
+                radius_str = input("Escribe el radio del círculo: ")
+                radius = float(radius_str)
+                area = pi*radius**2
+                print("El area del círculo es:", area)
+            area_circle()
 
+        # break option
+        elif option == "4":
+            print("¡Hasta la vista!")
+            break
+
+        # else print error
+        else: print("Error: Por favor elija un valor correcto.")
