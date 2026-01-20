0bf31a2d87d928d6541169f1a4a1cfe1999b5039
Felipe <mmillanpelaez@danielcastelao.org>
2026-01-20 13:24:52 +0100

    changed some function names in Boletin5.py

--- boletines/Boletin5.py
+++ boletines/Boletin5.py
@@ -32,13 +32,20 @@ def ejercicio2():
             print("\nERROR: Introduzca una temperatura valida\n")
 
 # 3. Utiliza o programa anterior para xerar unha táboa de conversión de temperaturas, dende 0º F ata 120º F, de 10 en 10.
-print("Ejercicio 3.")
-#for celsius in range(0, 121, 10):
-    #= (5/9)*(celsius - 32)
- #   print (fahrenheit, celsius)
+def ejercicio3():
+    print("\n--- Ejercicio 3 ---")
+    print("Tabla conversion temperatura.")
+    #
+    # for celsius in range(0, 121, 10):
+    #     print (fahrenheit, celsius)
+    #
+    # #= (5/9)*(celsius - 32)
+    #
 
 # 4. Escribir un programa que imprima tódolos números pares entre dous números que se lle pidan o usuario.
-print("Ejercicio 4.")
+def ejercicio4():
+    print("\n--- Ejercicio 4 ---")
+    print("Numeros pares.")
 
 # 5. Escribir un programa que reciba un número n por parámetro e imprima os primeiros n números triangulares, xunto co seu índice. Os números triangulares obteñense mediante a suma dos números naturales dende 1 ata n. É dicir, si se piden os primeros 5 números triangulares, o programa debe imprimir:
 #   1 - 1
@@ -47,48 +54,59 @@ print("Ejercicio 4.")
 #   4 - 10
 #   5 - 15
 #   Nota: facelo usando e sen usar a ecuación n ∗ (n + 1) / 2. Cal realiza máis operacións?
-print("Ejercicio 5.")
+def ejercicio5():
+    print("\n--- Ejercicio 5 ---")
+    print("Tabla conversion temperatura.")
+
 
 # 6. Escribir un programa que tome unha cantidade m de valores ingresados polo usuario, a cada un lle calcule o factorial e imprima o resultado xunto co número de orden correspondiente.
-print("Ejercicio 6.")
-
+def ejercicio6():
+    print("\n--- Ejercicio 6 ---")
+    print("Calcular factorial.")
 # 7. Escribir un programa que imprima por pantalla tódalas fichas de dominó, de unha por liña e sen repetir.
-print("Ejercicio 7.")
-def domino ():
-    for n in range(7):
-        for m in range(n, 7):
-            print("La ficha es:", n, '|', m)
-domino()
+def ejercicio7():
+    print("Ejercicio 7.")
+    def domino ():
+        for n in range(7):
+            for m in range(n, 7):
+                print("La ficha es:", n, '|', m)
+    domino()
 # 8. Modificar o programa anterior para que poida xerar fichas dun xogo que pode ter números de 0 a n.
-print("Ejercicio 8.")
-def generador_fichas(n):
-    for i in range(n+1):
-        for j in range(i, n+1):
-            print("La ficha es:", i, '|', j)
+def ejercicio8():
+    print("Ejercicio 8.")
+    def generador_fichas(n):
+        for i in range(n+1):
+            for j in range(i, n+1):
+                print("La ficha es:", i, '|', j)
 
-try:
-    m = int(input("Escribe el número máximo de la ficha: "))
-    generador_fichas(m)
-except ValueError:
-    print("Error: Por favor escribe un número entero.")
+    try:
+        m = int(input("Escribe el número máximo de la ficha: "))
+        generador_fichas(m)
+    except ValueError:
+        print("Error: Por favor escribe un número entero.")
 # 9. Calcula a cantidade de números negativos, positivos e ceros que hai nun grupo de 10 números enteiros.
-print("Ejercicio 9.")
+def ejercicio9():
+    print("Ejercicio 9.")
 
 # 10. Deseña un programa que calcule o área dun rectángulo cuxa base e altura pides por teclado. Asegúrate que estes valores sexan positivos, para eso valida os datos.
-print("Ejercicio 10.")
+def ejercicio10():
+    print("Ejercicio 10.")
 
 # 11. Codifica un programa que solicite un número e visualice a táboa de multiplicar dese número. O programa seguirá pedindo números ata que o usuario pulse o número cero.
-print("Ejercicio 11.")
+def ejercicio11():
+    print("Ejercicio 11.")
 
 # 12. Codifica un programa que lea o soldo de cada un dos traballadores dunha empresa e obteña o número deles que ganan entre 1000 e 1750 €, ámbolos dous incluídos e, a porcentaxe de traballadores que ganan menos de 1000 €. Tendo en conta que non se coñece con antelación o numero de traballadores da empresa e non se admiten os soldos negativos (utiliza como condición de fin un soldo 0).
-print("Ejercicio 12.")
+def ejercicio12():
+    print("Ejercicio 12.")
 
 # 13. Solicita o usuario un número n e debuxa un triángulo de base e altura n. Si o usuario teclea o número 4 o triángulo sería da seguinte forma:
 #       *
 #      * *
 #     * * *
 #    * * * *
-print("Ejercicio 13.")
+def ejercicio13():
+    print("Ejercicio 13.")
 
 # 14. Codificar o programa que solicite 10 números por consola e calcule a súa suma. Si o usuario introduce en calquera momento o número 999, deixa de solicitar máis números e presenta o valor da súma acadada ata ese momento.
 
@@ -101,7 +119,7 @@ def salir():
 # NOTA: No uses paréntesis () en las funciones aquí, solo el nombre.
 OPCIONES_MENU = {
     "1":  ("Imprimir rango de números", ejercicio1),
-    "2":  ("Ejercicio 2", ejercicio2),
+    "2":  ("Conversor Fahrenheit > Celsius.", ejercicio2),
     "3":  ("Ejercicio 3", ejercicio3),
     "4":  ("Ejercicio 4", ejercicio4),
     "5":  ("Ejercicio 5", ejercicio5),
