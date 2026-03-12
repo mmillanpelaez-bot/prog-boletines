e7e97d4d2db271031b055b2d520bd685eedeef5c
Felipe <mmillanpelaez@danielcastelao.org>
2026-03-12 14:43:34 +0100

    added Boletin9.py, Boletin10.py, Boletin11.py, Boletin12.py

---
+++ boletines/Boletin10.py
@@ -0,0 +1,26 @@
+# Boletín 10. Excepcións
+
+def ejercicio1():
+    """
+    1. Dadas as clases mostradas no diagrama UML (Persoa, Deportista), codificar os
+    métodos setter para que si o crear o obxecto do tipo persoa se introduce un DNI non
+    válido xere unha excepción do tipo DniNonValido.
+    Codificar o méthodo setLicenza para que verifique que o formato da licenza sexa
+    correcto. O formato da licenza (aaaadddnnnnnn) é da seguinte maneira :
+    ● aaaa, 4 números que representan o ano en curso.
+    ● ddd, abreviatura do deporte (fut fútbol, bal balocesto, etc).
+    ● nnnnnn, 6 números do número único de licenza.
+    """
+    pass
+
+def ejercicio2():
+    """
+    2. Adaptar a clase Data, para que só admita datos correctos a hora de introducir os
+    días, meses e anos, e que xere unha excepción si os datos non son correctos
+    (FormatoDataError). As condicións para que os datos sexan correctos son:
+    ● O número de día entre 1 e 28, 29, 30 e 31 (dependendo do mes e o ano). Ten
+    que verificar si é bisesto.
+    ● O número do mes entre 1 e 12.
+    ● O número de ano entre 1970 e 2999.
+    """
+    pass
